9dbc08479260b2ec83cb4e589a1726914315e86d
Emilse <emilse@gmail.com>
2019-09-16 23:33:36 -0300

    ...

---
+++ javascript_01/index.js
@@ -0,0 +1,26 @@
+var alumnos = [
+    {
+        name: 'joaquin',
+        lastname: 'soria',
+        age: '23',
+        work: 'frelance',
+        school: 'rollingcode'
+    },
+    {
+        name: 'federico',
+        lastname: 'soria',
+        age: '30',
+        work: 'factory',
+        school: 'rollingcode'
+    },
+    {
+        name: 'luis',
+        lastname: 'vasquez',
+        age:'29',
+        work: 'office',
+        school: 'rollingcode'
+    },
+
+]; 
+
+console.log(alumnos);
