d6627bdce611ff248c8511a0336c5d40e45e71ed
joaquin soria <quino_groxx@hotmail.com>
2019-09-18 23:15:35 -0300

    ....

--- javascript_01/index.js
+++ javascript_01/index.js
@@ -2,7 +2,7 @@ var alumnos = [
     {
         name: 'joaquin',
         lastname: 'soria',
-        age: '23',
+        age: '22',
         work: 'frelance',
         school: 'rollingcode'
     },
@@ -23,4 +23,85 @@ var alumnos = [
 
 ]; 
 
-console.log(alumnos);
+console.log(alumnos); 
+
+var familia = [
+    {
+        name:'susana' ,
+        lastname: 'rodriguez' ,
+        age:'63' ,
+        work: 'housewife',
+        hobbies: [
+            {
+                type: 'paint',
+            },
+            {
+                type: 'read',
+            },
+            {
+                type: 'facebook',
+            },
+        ]
+    },
+    {
+        name: 'luis',
+        lastname: 'soria',
+        age: '58',
+        work: 'retired',
+        hobbies: [
+            {
+                type: 'paint',
+            },
+            {
+                type: 'read',
+            },
+            {
+                type: 'facebook',
+            },
+        ]
+    },
+    {
+        name: 'joaquin',
+        lastname: 'soria',
+        age: '22',
+        work: 'frelance',
+        hobbies: [
+            {
+                type: 'paint',
+            },
+            {
+                type: 'read',
+            },
+            {
+                type: 'facebook',
+            },
+        ]
+        
+        
+    },
+    {
+        name: 'federico',
+        lastname: 'soria',
+        age: '30',
+        work: 'factory',
+        hobbies: [
+            {
+                type: 'paint',
+            },
+            {
+                type: 'read',
+            },
+            {
+                type: 'facebook',
+            },
+        ]
+    },
+];
+
+for(var i = 0; i<familia.length; i++) {
+    console.log('familiares de', familia[i].name);
+    for(var j = 0; j< familia[i].hobbies.length; j++){
+        console.log(familia[i].hobbies[j].type);
+    }
+    console.log('________________')   
+}
