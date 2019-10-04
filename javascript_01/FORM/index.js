929ee09ccd65b39d06d3d4094cb0fcb8c90a3a4d
Joaquin Soria <quino_groxx@hotmail.com>
2019-10-04 19:33:33 -0300

    ..

--- javascript_01/FORM/index.js
+++ javascript_01/FORM/index.js
@@ -1,8 +1,43 @@
+let i= 0;
+
 const processForm = (event) => {
     event.preventDefault();
     const title = document.getElementById('title').value;
-    if (title === '') {
-        alert('campo obligatorio');
-    }
     console.log(title);
+}
+
+const addmenber = () => {
+    const el = document.getElementById('familymenber');
+    const group = createGroup(i);
+    el.appendChild(group);
+    i++;
+}
+
+const createElement = (type, classname, placeholder='', id) => {
+    const el = document.createElement(type);
+    if (placeholder !=='' && type === 'input') {
+        el.placeholder = placeholder; 
+    }
+    if (type === 'input') {
+        el.id = placeholder + '_input_' + id;
+    }
+    el.classname = classname;
+    return el;
+}
+
+const createGroup = (id) => {
+    const row = createElement('div', 'row',  id);
+    const col1 = createElement('div', 'col-4');
+    const col2 = createElement('div', 'col-4');
+    const col3 = createElement('div', 'col-3');
+    const input1 = createElement('input', 'form-control', 'Name', id);
+    const input2 = createElement('input', 'form-control', 'Lastname', id);
+    const input3 = createElement('input', 'form-control', 'Document', id);
+    col1.appendChild(input1);
+    col2.appendChild(input2);
+    col3.appendChild(input3);
+    row.appendChild(col1);
+    row.appendChild(col2);
+    row.appendChild(col3);
+    return row;
 }
