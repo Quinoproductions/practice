662d1efb5734c9c0fbec0c91fa01764959dd3324
Gonzalofl <>
2019-10-04 23:11:36 -0300

    ...

--- javascript_01/FORM/index.js
+++ javascript_01/FORM/index.js
@@ -21,23 +21,27 @@ const createElement = (type, classname, placeholder='', id) => {
     if (type === 'input') {
         el.id = placeholder + '_input_' + id;
     }
-    el.classname = classname;
+    el.className = classname;
     return el;
 }
 
 const createGroup = (id) => {
-    const row = createElement('div', 'row',  id);
-    const col1 = createElement('div', 'col-4');
-    const col2 = createElement('div', 'col-4');
-    const col3 = createElement('div', 'col-3');
+    const row = createElement('div', 'row');
+    const col1 = createElement('div', 'col-2');
+    const col2 = createElement('div', 'col-2');
+    const col3 = createElement('div', 'col-2');
+    const col4 = createElement('div', 'col-2');
     const input1 = createElement('input', 'form-control', 'Name', id);
     const input2 = createElement('input', 'form-control', 'Lastname', id);
     const input3 = createElement('input', 'form-control', 'Document', id);
+    const input4 = createElement('input', 'form-control', 'cell-number', id);
     col1.appendChild(input1);
     col2.appendChild(input2);
     col3.appendChild(input3);
+    col4.appendChild(input4);
     row.appendChild(col1);
     row.appendChild(col2);
     row.appendChild(col3);
+    row.appendChild(col4);
     return row;
 }
